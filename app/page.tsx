d94691d9801b5af37334630f58345b3a1ffcbd3b
Lucas <contato.developerlucas@gmail.com>
2024-06-17 12:34:23 -0300

    tests

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ThemeToggle from '@/components/ThemeToggle/themeToggle'
 export default function HomeToggle() {
   return (
     <div className="flex h-screen items-center justify-center gap-3">
-      <h1 className="font-bold">TOGGLE THEME!</h1>
+      <h1 className="font-bold">TOGGLE THEME!!</h1>
       <div className="h-6 w-[1px] bg-dark dark:bg-white" />
       <ThemeToggle />
     </div>
